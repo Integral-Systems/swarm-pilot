776d415debfc10f7e6f2f7a5751ac88ad28e0b3c
Gradlon von Känel <gradlon@integral-systems.ch>
2025-04-23 21:31:48 +0200

    improved log

--- src/helpers.ts
+++ src/helpers.ts
@@ -206,7 +206,7 @@ export function calculate_scaling(data: { usage: Usage, service_spec: DockerServ
                         });
                         yield* Effect.logInfo(`Service ${d.usage.service_name} needs scaling from ${current_scaling} to ${scale} replicas.`);
                     } else {
-                        yield* Effect.logInfo(`No scaling needed for service ${d.usage.service_name}, current scaling is ${current_scaling}. CPU: ${cpu_percent}%|${cpuLimit}%, Memory: ${memory_percent}%|${memoryLimit}%`);
+                        yield* Effect.logInfo(`No scaling needed for service ${d.usage.service_name}, current scaling is ${current_scaling}. CPU: ${cpu_percent}%|${cpuUsage}${cpuLimit}, Memory: ${memory_percent}%|${memoryUsage}/${memoryLimit}`);
                     }
                 }
             }
